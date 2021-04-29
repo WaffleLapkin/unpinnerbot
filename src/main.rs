ec5f4f3f05de52bcf170caccfb07ec8e361775bf
Waffle <waffle.lapkin@gmail.com>
2021-04-29 12:18:07 +0300

    Change debouncing mechanism
    This patch changes the way we debounce updates.
    
    Instead of updating global `HashMap` (in a `Arc<Mutex<_>>`) we now use (tokio)
    channels to talk to 'worker' and schedule message unpinning. The HashMap now
    stored localy in the worker and periodically updated from the channel using
    `read_from_rx`.
    
    `read_from_rx` reads all present tasks from the channel and appends it to the
    HashMap. It also tries to avoid DOS posibility by not allowing to grow HashMap.
    If there are no tasks in the HashMap nor in the channel, it blocks (async),
    waiting for a task.
    
    Worker now uses waiting, instead of `timeout`s. IMO it"s more readable this way.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::collections::HashMap;
-use std::sync::Arc;
+#![feature(hash_drain_filter)]
+
+use futures::{Future, FutureExt};
 use std::time::Duration;
+use std::{collections::HashMap, time::Instant};
 use teloxide::types::{MessageKind, User};
 use teloxide::{
     adaptors::{throttle::Limits, Throttle},
@@ -8,10 +10,7 @@ use teloxide::{
     ApiError, RequestError,
 };
 use teloxide::{prelude::*, types::Me};
-use tokio::sync::{
-    mpsc::{self, Sender},
-    Mutex,
-};
+use tokio::sync::mpsc::{self, Sender};
 
 type Bot = AutoSend<Throttle<teloxide::Bot>>;
 
@@ -25,76 +24,30 @@ async fn main() {
     pretty_env_logger::init();
     log::info!("Starting unpinnerbot...");
 
-    let (debounce_tx, mut debounce_rx) = mpsc::channel(100);
+    let (worker_tx, worker_rx) = mpsc::channel(100);
 
     let bot = teloxide::Bot::from_env()
         .throttle(Limits::default())
         .auto_send();
     let Me { user: bot_user, .. } = bot.get_me().await.unwrap();
 
-    let channel_map: Arc<Mutex<HashMap<i64, i32>>> = Default::default();
-
-    let debounce_task = tokio::spawn({
-        let bot = bot.clone();
-        let channel_map = Arc::clone(&channel_map);
-        let timeout = Duration::from_secs(1);
-        let mut is_there_work = false;
-
-        async move {
-            loop {
-                match tokio::time::timeout(timeout, debounce_rx.recv()).await {
-                    Ok(Some(())) => {
-                        log::debug!("debouncing");
-                        is_there_work = true;
-                    }
-                    Ok(None) => {
-                        log::debug!("stopping");
-                        break;
-                    }
-                    Err(_) if is_there_work => {
-                        log::debug!("done debouncing");
-
-                        let mut channel_map = channel_map.lock().await;
-                        for (&group, &msg_id) in channel_map.iter() {
-                            if let Err(e) = bot.unpin_chat_message(group).message_id(msg_id).await {
-                                if let RequestError::ApiError {
-                                    kind: ApiError::NotEnoughRightsToManagePins,
-                                    ..
-                                } = e
-                                {
-                                    bot.send_message(group, "Failed to unpin this message. Please, grant me Pin Messages permission to work properly.").reply_to_message_id(msg_id).await.log_on_error().await;
-                                } else {
-                                    log::error!("Error: {}", e);
-                                }
-                            }
-                        }
-                        channel_map.clear();
-
-                        is_there_work = false;
-                    }
-                    _ => {}
-                }
-            }
-        }
-    });
+    let unpin_task = tokio::spawn(worker(&bot, worker_rx));
 
     Dispatcher::new(bot)
         .messages_handler(move |mut rx: DispatcherHandlerRx<_, Message>| async move {
-            let channel_map = Arc::clone(&channel_map);
-
             while let Some(update) = rx.recv().await {
                 let message = &update.update;
                 if message.chat.is_private() {
                     handle_private(update).await.log_on_error().await;
                 } else if message.chat.is_supergroup() {
-                    handle_supergroup(update, &debounce_tx, &bot_user, &*channel_map).await;
+                    handle_supergroup(update, &worker_tx, &bot_user).await;
                 }
             }
         })
         .dispatch()
         .await;
 
-    debounce_task.await.unwrap();
+    unpin_task.await.unwrap();
 }
 
 async fn handle_private(cx: UpdateWithCx<Bot, Message>) -> Result<(), RequestError> {
@@ -104,9 +57,8 @@ async fn handle_private(cx: UpdateWithCx<Bot, Message>) -> Result<(), RequestErr
 
 async fn handle_supergroup(
     cx: UpdateWithCx<Bot, Message>,
-    debounce_tx: &Sender<()>,
+    worker_tx: &Sender<(i64, (i32, Instant))>,
     bot_user: &User,
-    channel_map: &Mutex<HashMap<i64, i32>>,
 ) {
     let message = &cx.update;
     match &message.kind {
@@ -117,11 +69,86 @@ async fn handle_supergroup(
         }
         MessageKind::Common(message_common) => {
             if let Some(User { id: 777000, .. }) = message_common.from {
-                debounce_tx.send(()).await.unwrap();
-                let mut channel_map = channel_map.lock().await;
-                channel_map.insert(message.chat.id, message.id);
+                worker_tx
+                    .send((message.chat.id, (message.id, Instant::now())))
+                    .await
+                    .unwrap();
             }
         }
         _ => {}
+    }
+}
+
+fn worker(
+    bot: &AutoSend<Throttle<teloxide::Bot>>,
+    mut rx: mpsc::Receiver<(i64, (i32, Instant))>,
+) -> impl Future<Output = ()> {
+    let bot = bot.clone();
+
+    async move {
+        let mut rx_closed = false;
+        const TIMEOUT: Duration = Duration::from_secs(1);
+
+        // Chat id => (id of the last message from linked channel, time it was received)
+        let mut tasks: HashMap<i64, (i32, Instant)> = HashMap::with_capacity(64);
+
+        while !rx_closed || !tasks.is_empty() {
+            read_from_rx(&mut rx, &mut tasks, &mut rx_closed).await;
+            let now = Instant::now();
+            let timeout_back = now - TIMEOUT;
+
+            for (group, msg_id) in tasks
+                .drain_filter(|_, (_, t)| *t < timeout_back)
+                .map(|(g, (m, _))| (g, m))
+            {
+                unpin(&bot, group, msg_id).await;
+            }
+        }
+    }
+}
+
+async fn read_from_rx(
+    rx: &mut mpsc::Receiver<(i64, (i32, Instant))>,
+    tasks: &mut HashMap<i64, (i32, Instant)>,
+    rx_is_closed: &mut bool,
+) {
+    if tasks.is_empty() {
+        match rx.recv().await {
+            Some((chat_id, value)) => {
+                tasks.insert(chat_id, value);
+            }
+            None => *rx_is_closed = true,
+        }
+    }
+
+    // Don't grow queue bigger than the capacity to limit DOS posibility
+    while tasks.len() < tasks.capacity() {
+        // FIXME: https://github.com/tokio-rs/tokio/issues/3350
+        match rx.recv().now_or_never() {
+            Some(Some((chat_id, new_task))) => {
+                // Insert the latest message id into jobs list
+                let task = tasks.entry(chat_id).or_insert(new_task);
+                *task = std::cmp::max_by_key(*task, new_task, |(mid, _)| *mid);
+            }
+            Some(None) => *rx_is_closed = true,
+            // There are no items in queue.
+            None => break,
+        }
+    }
+}
+
+async fn unpin(bot: &AutoSend<Throttle<teloxide::Bot>>, group: i64, msg_id: i32) {
+    if let Err(e) = bot.unpin_chat_message(group).message_id(msg_id).await {
+        match e {
+            RequestError::ApiError { kind: ApiError::NotEnoughRightsToManagePins, ..} => {
+                bot
+                    .send_message(group, "Failed to unpin this message. Please, grant me Pin Messages permission to work properly.")
+                    .reply_to_message_id(msg_id)
+                    .await
+                    .log_on_error()
+                    .await
+            },
+            _ => log::error!("Error: {}", e),
+        }
     }
 }
